e4bc6a490f0111dde94388e0653dd8605b6f1da2
UsernameFull <tohowtodoit@gmail.com>
2026-05-28 14:15:20 +0800

    vllm

--- tests/third_party/vllm/conftest.py
+++ tests/third_party/vllm/conftest.py
@@ -3,14 +3,11 @@ import os
 
 if os.environ.get("ROLL_NPU_CI") == "1":
     collect_ignore = [
-        "test_abort.py",
         "test_add_requests.py",
         "test_collective_rpc.py",
         "test_fp8.py",
         "test_fp8_perf.py",
         "test_sleep_level.py",
-        "test_vllm_local_actor.py",
-        "test_vllm_local_async.py",
         "test_vllm_mem_oom.py",
         "vllm_generate_test.py", # npu server has no MATH_train_reformat_241225.json
     ]

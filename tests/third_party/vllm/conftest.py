a368961720dc562c9206470880a5efa99fffe30f
UsernameFull <tohowtodoit@gmail.com>
2026-05-28 11:22:24 +0800

    xx

--- tests/third_party/vllm/conftest.py
+++ tests/third_party/vllm/conftest.py
@@ -9,8 +9,8 @@ if os.environ.get("ROLL_NPU_CI") == "1":
         "test_fp8.py",
         "test_fp8_perf.py",
         "test_sleep_level.py",
-        "test_vllm_local.py",
         "test_vllm_local_actor.py",
         "test_vllm_local_async.py",
         "test_vllm_mem_oom.py",
+        "vllm_generate_test.py", # npu server has no MATH_train_reformat_241225.json
     ]

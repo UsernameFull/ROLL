59049a4b70279b80868690d32042ac15e413352e
UsernameFull <tohowtodoit@gmail.com>
2026-05-30 19:24:43 +0800

    xx

--- tests/third_party/sglang/conftest.py
+++ tests/third_party/sglang/conftest.py
@@ -7,6 +7,7 @@ if os.environ.get("ROLL_NPU_CI") == "1":
     collect_ignore = [
         "test_abort.py",
         "test_abort_grpc.py",
+        "test_abort_http.py",
         "test_fp8.py",
     ]
     if os.environ.get("ROLL_NPU_SGLANG_ABORT_ENGINE") == "1":

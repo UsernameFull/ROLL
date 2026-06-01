25a304a24c3794398bda73ac12fee87b95d5a398
UsernameFull <tohowtodoit@gmail.com>
2026-06-01 16:13:07 +0800

    Default MCore qk layernorm to false

--- mcore_adapter/src/mcore_adapter/models/model_config.py
+++ mcore_adapter/src/mcore_adapter/models/model_config.py
@@ -296,6 +296,10 @@ class McaModelConfig(TransformerConfig, PretrainedConfig):
             "choices": ["local", "transformer_engine"],
         },
     )
+    qk_layernorm: bool = field(
+        default=False,
+        metadata={"help": "Apply layernorm to query and key projections."},
+    )
 
     def __post_init__(self):
         if current_platform.is_npu() and self.transformer_impl == "transformer_engine":
